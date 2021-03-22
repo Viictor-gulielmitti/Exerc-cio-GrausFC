5947ae879f250a2763cef5e3819e7e501ba5ac56
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-21 21:38:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GRAUS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String entrada;
+            double C, F;
+            Console.WriteLine(".................[GRAUS].........................");
+            Console.WriteLine ( " Digite a temperatura (em ° F): " );
+            entrada  =  Console.ReadLine();
+            C = Convert.ToDouble (entrada);
+            F = (F: 32 / 1,8);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine ("$ \n Equivalência");
+            Console.WriteLine ($" Essa temperatura em °C: { "C [GRAUS]"}");
+            Console.WriteLine ("................[GRAUS].........................");
+            
